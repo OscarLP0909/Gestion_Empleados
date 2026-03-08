8fb8a609b10803295b399e852f2b6d59180c8dc0
OscarLP09 <oscar.s352656@cesurformacion.com>
2026-03-08 22:00:08 +0100

    Update contractRouter.ts

--- backend/src/routes/contractRouter.ts
+++ backend/src/routes/contractRouter.ts
@@ -1,11 +1,12 @@
 import { Router } from "express";
 import { ensureAuthenticated } from "../middlewares/auth/jwt.js";
-import { createContract, getContracts } from "../controllers/contractController.js";
+import { createContract, getContractById, getContracts } from "../controllers/contractController.js";
 
 const router = Router();
 
 router.post("/", ensureAuthenticated, createContract);
 router.get("/", ensureAuthenticated, getContracts);
+router.get("/contract/:id", ensureAuthenticated, getContractById);
 
 
 export default router;
