e2bad50e797c38fb1af0dd9aa97921862f226961
OscarLP09 <oscar.s352656@cesurformacion.com>
2026-03-10 15:59:49 +0100

    RouterContract terminado

--- backend/src/controllers/contractController.ts
+++ backend/src/controllers/contractController.ts
@@ -5,7 +5,7 @@ import { Employee } from "../db/models/Employee.js";
 
 export const createContract = async(req: Request, res: Response, next: NextFunction) => {
     try {
-        const { employeeId, contractType, temporaryType, workdayType, salaryType, salaryAmount, startDate, endDate, department, category, position } = req.body;
+        const { employeeId, contractType, temporaryType, workdayType, salaryType, salaryAmount, startDate, endDate, department, category, position, status } = req.body;
 
         if (typeof employeeId !== "string" || employeeId.trim() === "") {
             res.status(400).json({message: "Employee ID is requested"});
@@ -36,7 +36,8 @@ export const createContract = async(req: Request, res: Response, next: NextFunct
             endDate: endDate || null,
             department: department.trim(),
             category: category.trim(),
-            position: position.trim()
+            position: position.trim(),
+            status: status.trim()
         });
         await contract.save();
         res.status(201).json(contract);
@@ -97,4 +98,122 @@ export const deleteContract = async (req: Request, res: Response, next: NextFunc
     } catch (error) {
         next(error);
     }
-}
+};
+
+export const getContractsOfEmployee = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const { employeeId } = req.params;
+        if (typeof(employeeId) !== "string" || employeeId.trim() === "") {
+            res.status(400).json({ message: "ID has to be a string"});
+            return;
+        }
+        if(!Types.ObjectId.isValid(employeeId)) {
+            res.status(400).json({ message: "Invalid ID format"});
+            return;
+        }
+        const contracts = await Contract.find({employeeId: employeeId});
+        if(contracts.length === 0) {
+            res.status(404).json({message: "The Employee doesn't have any Contract"});
+            return;
+        }
+        res.status(200).json(contracts);
+    } catch (error) {
+        next(error);
+    }
+};
+
+export const getContractActiveOfEmployee = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const {employeeId} = req.params;
+        if (typeof(employeeId) !== "string" || employeeId.trim() === "") {
+            res.status(400).json({ message: "ID has to be a string"});
+            return;
+        }
+        if(!Types.ObjectId.isValid(employeeId)) {
+            res.status(400).json({ message: "Invalid ID format"});
+            return;
+        }
+        const contract = await Contract.find({employeeId: employeeId, status: "ACTIVO"});
+        if(contract.length === 0) {
+            res.status(404).json({message: "This Employee doesn't have an active contract"});
+            return;
+        }
+        res.status(200).json(contract);
+    } catch (error) {
+        next(error);
+    }
+};
+
+export const updateContract = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const { id } = req.params;
+        const { employeeId, contractType, temporaryType, workdayType, salaryType, salaryAmount, startDate, endDate, department, category, position, status } = req.body;
+
+        if (typeof id !== "string" || id.trim() === "") {
+            res.status(400).json({ message: "ID has to be a non-empty string" });
+            return;
+        }
+
+        if (!Types.ObjectId.isValid(id)) {
+            res.status(400).json({ message: "Invalid contract id format" });
+            return;
+        }
+
+        const contract = await Contract.findById(id);
+        if (!contract) {
+            res.status(404).json({ message: "Contract not found" });
+            return;
+        }
+
+        const updatedContract = await Contract.findByIdAndUpdate(
+            id,
+            {
+                employeeId: employeeId?.trim() ?? contract.employeeId,
+                contractType: contractType ?? contract.contractType,
+                temporaryType: temporaryType ?? contract.temporaryType,
+                workdayType: workdayType ?? contract.workdayType,
+                salaryType: salaryType ?? contract.salaryType,
+                salaryAmount: salaryAmount ?? contract.salaryAmount,
+                startDate: startDate ?? contract.startDate,
+                endDate: endDate ?? contract.endDate,
+                department: department?.trim() ?? contract.department,
+                category: category?.trim() ?? contract.category,
+                position: position?.trim() ?? contract.position,
+                status: status ?? contract.status
+            },
+            { new: true }
+        );
+        res.status(200).json(updatedContract);
+    } catch (error) {
+        next(error);
+    }
+};
+
+export const updateStatus = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const { id } = req.params;
+        const { status } = req.body;
+
+        if (typeof id !== "string" || id.trim() === "") {
+            res.status(400).json({ message: "ID has to be a non-empty string" });
+            return;
+        }
+
+        if (!Types.ObjectId.isValid(id)) {
+            res.status(400).json({ message: "Invalid contract id format" });
+            return;
+        }
+
+        const contract = await Contract.findById(id);
+        if(!contract) {
+            res.status(404).json({message: "Contract not found"});
+            return;
+        }
+        const updateStatus = await Contract.findByIdAndUpdate(id, {
+            status: status.trim() ?? contract.status,
+        });
+        res.status(200).json(updateStatus);
+    } catch (error) {
+        next(error);
+    }
+};
--- backend/src/routes/contractRouter.ts
+++ backend/src/routes/contractRouter.ts
@@ -1,13 +1,17 @@
 import { Router } from "express";
 import { ensureAuthenticated } from "../middlewares/auth/jwt.js";
-import { createContract, deleteContract, getContractById, getContracts } from "../controllers/contractController.js";
+import { createContract, deleteContract, getContractActiveOfEmployee, getContractById, getContracts, getContractsOfEmployee, updateContract, updateStatus } from "../controllers/contractController.js";
 
 const router = Router();
 
 router.post("/", ensureAuthenticated, createContract);
 router.get("/", ensureAuthenticated, getContracts);
-router.get("/contract/:id", ensureAuthenticated, getContractById);
-router.delete("/contract/:id", ensureAuthenticated, deleteContract);
+router.put("/:id", ensureAuthenticated, updateContract);
+router.get("/:id", ensureAuthenticated, getContractById);
+router.delete("/:id", ensureAuthenticated, deleteContract);
+router.patch("/status/:id", ensureAuthenticated, updateStatus);
+router.get("/employee/active/:employeeId", ensureAuthenticated, getContractActiveOfEmployee);
+router.get("/employee/:employeeId", ensureAuthenticated, getContractsOfEmployee);
 
 
 export default router;
